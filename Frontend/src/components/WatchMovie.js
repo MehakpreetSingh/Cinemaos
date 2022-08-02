68f76d9b44cb6d5b661da45f1a635c4d54c3b7cd
Mehak <mehakbrar811@gmail.com>
2022-08-02 16:04:40 +0530

    New Commit

--- Frontend/src/components/WatchMovie.js
+++ Frontend/src/components/WatchMovie.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import ReactPlayer from 'react-player'
 import { useParams } from 'react-router-dom';
-import ComingSoon from './ComingSoon';
+
 
 const WatchMovie = () => {
     const [dbinfo, setDbinfo] = useState({});
@@ -22,12 +22,28 @@ const WatchMovie = () => {
         setDbinfo(data);
     }
     dbData() ;
-    setTimeout(() => {
-        setLoading(false) ;
-    }, 800);
-    }, [id])
+    var x = document.getElementById("loading-bar");
+        for (let i = 0; i < 4; i++) {
+            setTimeout(() => {
+                x.classList.remove(`w-[${(i) * 25}%]`);
+                x.classList.add(`w-[${(i + 1) * 25}%]`);
+            }, 100);
+        }
+        x.classList.remove("w-[100%]");
+
+        setTimeout(() => {
+            setLoading(false);
+        }, 350);
+        setTimeout(() => {
+            x.classList.add("w-0")
+        }, 400);
+    }, [])
     return (
         <>
+            <div className='h-[2px] w-full z-[99999999] absolute top-[63px]'>
+                <div id="loading-bar" className='transition-all w-[0%] h-[2px] bg-red-800'>
+                </div>
+            </div>
     {!loading && 
         <div className='h-screen flex justify-center items-center'>
             {
